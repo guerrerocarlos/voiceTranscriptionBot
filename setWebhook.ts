063cf5c691d95977e143e07b813f810479d7f182
Carlos Guerrero <git@carlosguerrero.com>
2021-10-19 23:48:52 +0100

    Add setWebhook script

---
+++ setWebhook.ts
@@ -0,0 +1,13 @@
+import { Telegraf } from "telegraf";
+
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN, {
+  telegram: { webhookReply: true },
+});
+
+bot.telegram.setWebhook(
+  "https://ilpifex4si.execute-api.us-east-2.amazonaws.com/dev/telegram"
+);
