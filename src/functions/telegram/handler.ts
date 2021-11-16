5432bf1bf736111f53934cc776a195fb4e87ce86
Carlos Guerrero <git@carlosguerrero.com>
2021-11-16 14:10:49 +0000

    20 secs timeout for transcription

--- src/functions/telegram/handler.ts
+++ src/functions/telegram/handler.ts
@@ -52,8 +52,6 @@ bot.on("voice", async function (ctx) {
 
   console.log("FILE LINK", fileLink);
 
-  return {}
-
   try {
     let interval = setInterval(() => {
       ctx.telegram.editMessageText(
@@ -78,7 +76,7 @@ bot.on("voice", async function (ctx) {
       await googleTranscribe(fileLink),
       timeout,
     ]) as any;
-
+    
     clearInterval(interval);
 
     console.log("result", JSON.stringify(result, null, 2));
